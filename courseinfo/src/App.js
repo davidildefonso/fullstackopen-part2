1973aefff3d45cca781bd032980315bab2bf552d
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-11-20 19:05:04 -0500

    add ex 1.1

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React from 'react'
 
-const Hello = (props) => (
-	<p>  hello {props.name}! you are {props.age} years old </p>
+
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Content = (props) => (
+		<>
+   		<p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+		</>
+)
+
+const Total = (props) => (
+ 	<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 )
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-	const now = new Date();
-	const a = 10;
-	const b = 12;
+  return (
+    <div>
+			<Header   course={course} />
 
-	return (
-			<>
-				<Hello name="David"  age="32" />
-				<p> how es {now.toString()} </p>
-				<Hello name="Sebastian" age="9"  />
-				<p> {a} + {b} = { a + b } </p>
-				<Hello  name="Magaly" age="37"  />
-				<Hello name="Serafina" age="13" />
-			</>
-			
-	)
-	
+			<Content   exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}
+				 				part1 = {part1} part2 = {part2} part3 = {part3}  />
+
+			<Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}  />
+    </div>
+  )
 }
-	
-;
 
-export default App;
+export default App
